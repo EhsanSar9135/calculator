5c0e7b5576bf456f75adbb1c93bcfc1709faf3ee
ehsan saraee <ehsansaraee6570@yahoo.com>
2021-08-04 19:48:38 +0430

    Our App's UI has been Completed

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,9 +1,37 @@
 import React from "react";
-import { Container } from "../styles";
+import { Button, Container, Current, Previous, Screen } from "../styles";
 
 const Calculator = () => {
    return (
-        <Container />
+      <Container>
+         <Screen>
+            <Previous>10 +</Previous>
+            <Current>5</Current>
+         </Screen>
+         <Button gridSpan={2} control>
+            AC
+         </Button>
+         <Button control>DEL</Button>
+         <Button operation>÷</Button>
+         <Button>7</Button>
+         <Button>8</Button>
+         <Button>9</Button>
+         <Button operation>×</Button>
+         <Button>4</Button>
+         <Button>5</Button>
+         <Button>6</Button>
+         <Button operation>+</Button>
+         <Button>1</Button>
+         <Button>2</Button>
+         <Button>3</Button>
+         <Button operation>-</Button>
+         <Button control></Button>
+         <Button>0</Button>
+         <Button gridSpan={2} operation>
+            {" "}
+            ={" "}
+         </Button>
+      </Container>
    );
 };
 
